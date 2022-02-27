c22a8c841d292f2abf7fa98b6eb0f65419dffb37
proger-it <98544012+proger-it@users.noreply.github.com>
2022-02-28 00:01:10 +0300

    lab1
    lab1

---
+++ lab1/src/main/java/com/example/lab1/Lab1Application.java
@@ -0,0 +1,15 @@
+package com.example.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class Lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+	}
+
+}
