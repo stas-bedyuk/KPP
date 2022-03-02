5206dafaf5007591ee6acf134d114cbf22a86f7a
proger-it <98544012+proger-it@users.noreply.github.com>
2022-03-02 13:19:07 +0300

    Initial commit

--- lab1/src/main/java/com/example/lab1/Lab1Application.java
+++ lab1/src/main/java/com/example/lab1/Lab1Application.java
@@ -2,14 +2,12 @@ package com.example.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
 public class Lab1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+    }
 
 }
