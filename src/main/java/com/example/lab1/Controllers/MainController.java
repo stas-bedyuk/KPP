26b51b2382a8be1a9a2c275c540c75d4f471c706
stas-bedyuk <98544012+stas-bedyuk@users.noreply.github.com>
2022-03-28 10:22:47 +0200

    Add files via upload

--- src/main/java/com/example/lab1/Controllers/MainController.java
+++ src/main/java/com/example/lab1/Controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.example.lab1.Controllers;
+import com.example.lab1.Logger.MyLogger;
 import com.example.lab1.Repository;
 import com.example.lab1.Results.Result;
 import com.example.lab1.Triangle;
 import com.example.lab1.Validations.InputValidation;
+import org.apache.logging.log4j.Level;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,22 +17,19 @@ public class MainController {
     private final AtomicLong counter = new AtomicLong();
     Repository rep = new Repository();
         @GetMapping("/triangle")
-        public Result Get (
+        public Result Enter (
         @RequestParam(value = "side1", required = true, defaultValue = "3") int side1,
         @RequestParam(value = "side2", required = true, defaultValue = "4") int side2,
         @RequestParam(value = "side3", required = true, defaultValue = "5") int side3)
         {
             Triangle ThisTriangle = new Triangle(side1, side2, side3);
-            return rep.getParameters(ThisTriangle);
+            if(rep.isContain(ThisTriangle)) {
+                MyLogger.Log(Level.INFO,  "This Triangle is already in the chache");
+                return rep.getParameters(ThisTriangle);
+            }
+            else {
+                MyLogger.Log(Level.INFO,  "This Triangle is added in the chache");
+                return rep.addToMap(ThisTriangle,InputValidation.optionsValidation(counter.incrementAndGet(),ThisTriangle));
+            }
         }
-    @GetMapping("/chache")
-    public Result Enter (
-            @RequestParam(value = "side1", required = true) int side1,
-            @RequestParam(value = "side2", required = true) int side2,
-            @RequestParam(value = "side3", required = true) int side3)
-    {
-        Triangle ThisTriangle = new Triangle(side1, side2, side3);
-            return rep.addToMap(ThisTriangle,InputValidation.optionsValidation(counter.incrementAndGet(),ThisTriangle));
-    }
-
 }
