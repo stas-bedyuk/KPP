2ebcb33e623bf38d843d5b04bc92c277e4f572e7
stas-bedyuk <98544012+stas-bedyuk@users.noreply.github.com>
2022-04-24 17:10:04 +0200

    Add files via upload

---
+++ lab1/Repository.java
@@ -0,0 +1,27 @@
+package com.example.lab1;
+
+import com.example.lab1.Logger.MyLogger;
+import org.springframework.stereotype.Component;
+import com.example.lab1.Validations.Results.Result;
+import java.util.HashMap;
+import org.apache.logging.log4j.Level;
+
+@Component
+public class Repository {
+    private final HashMap<Triangle, Result> hashMap = new HashMap<>();
+
+    public boolean isContain(Triangle key) {
+        return hashMap.containsKey(key);
+    }
+
+    public Result addToMap(Triangle key, Result result) {
+        hashMap.put(key, result);
+        MyLogger.Log(Level.INFO, result + " is calculated");
+        return result;
+    }
+
+    public Result getParameters(Triangle key) {
+
+        return hashMap.get(key);
+    }
+}
